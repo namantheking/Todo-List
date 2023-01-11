b764df822908d53bc6af78f2ff49a6cef8fecc07
namantheking <120478194+namantheking@users.noreply.github.com>
2023-01-11 21:56:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://admin-naman:test123@cluster0.3divyrr.mongodb.net/todoListDB");
+mongoose.connect(process.env.MONGO);
 const itemsSchema = {
     name: {type: String, required: true},
     type: {type: String, required: true}
