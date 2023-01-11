a9a7b99feab6615c5952cb0ce65a019b7a761cd0
namantheking <epicgamernm@gmail.com>
2023-01-11 20:10:41 +0530

    Some editing in port

--- app.js
+++ app.js
@@ -122,7 +122,11 @@ app.post("/delete", function(req, res){
     })
 })
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
 
-app.listen(3000, function () {
-    console.log("Server running on port 3000");
+app.listen(port, function () {
+    console.log("Server up and running.");
 })
